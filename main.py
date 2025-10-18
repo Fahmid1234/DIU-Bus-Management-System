d63cc691f895c61b4b58b0f7f7caa703d55c4715
Fahmid1234 <142990312+Fahmid1234@users.noreply.github.com>
2025-10-18 21:41:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app.config['STATIC_URL_PATH'] = '/static'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'mdfahmidbinmostafa@gmail.com'
-app.config['MAIL_PASSWORD'] = 'glffppadhqigbvei'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
